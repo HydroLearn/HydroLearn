e11d69aca96f0280530bca80bc4343043b054c0b
crivet <c.rivet@louisiana.edu>
2018-08-21 08:20:23 -0500

    Modified several db settings to read from config file, as config may differ between prod/dev environments. now all email/database/sensitive keys are relocated to expected config file.

--- src/settings/database.py
+++ src/settings/database.py
@@ -5,22 +5,14 @@ from src.settings.config_reader import get_config_setting
 DATABASES = {
     'default': {
         'CONN_MAX_AGE': 0,
-        'ENGINE': 'django.db.backends.sqlite3',
-        'HOST': get_config_setting('DB_HOST'),
-        'NAME': get_config_setting('DB_NAME'),
+        'ENGINE': get_config_setting('DB_ENGINE'),
+        'PORT': get_config_setting('DB_PORT'),
+        'HOST': get_config_setting("DB_HOST"),
+        'NAME': get_config_setting("DB_NAME"),
         'USER': get_config_setting('DB_USER'),
         'PASSWORD': get_config_setting('DB_PASS'),
-        'PORT': '',
 
     }
 
-    # sample postgres setup ( to be used later
-    # 'default': {
-    #     'ENGINE': 'django.db.backends.postgresql',
-    #     'NAME': 'mydatabase',
-    #     'USER': 'mydatabaseuser',
-    #     'PASSWORD': 'mypassword',
-    #     'HOST': '127.0.0.1',
-    #     'PORT': '5432',
-    # }
+
 }
