ef92aca234337a3d1ec3187ee1b9714300d4e9f2
crivet <c.rivet@louisiana.edu>
2018-08-21 13:52:50 -0500

    BUGFIX: Email settings were incorrect, corrected this issue to correctly pull from the remote config file

--- src/settings/email_info.py
+++ src/settings/email_info.py
@@ -1,10 +1,11 @@
 from src.settings.config_reader import get_config_setting
 
 # currently setup with gmail, need to revise this to use Office356 from prod
-EMAIL_USE_TLS = True
-EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_USE_TLS = get_config_setting('EMAIL_USE_TLS')
+EMAIL_HOST = get_config_setting('EMAIL_HOST')
+EMAIL_PORT = get_config_setting('EMAIL_PORT')
 EMAIL_HOST_USER = get_config_setting('EMAIL_USER')
 EMAIL_HOST_PASSWORD = get_config_setting('EMAIL_PASS')
-EMAIL_PORT = 587
-DEFAULT_FROM_EMAIL = 'HydroLearn <accounts.no-reply@hydrolearn.org>'
+DEFAULT_FROM_EMAIL = get_config_setting('EMAIL_DEFAULT_FROM')
+
 
